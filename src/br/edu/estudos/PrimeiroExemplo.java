32f62edc35f44e02b09e0d290841c0d3623514a5
Júlio Santos <julio.santos@dextra-sw.com>
2021-02-18 22:18:36 -0300

    Alterei mais uma coisa aqui

--- src/br/edu/estudos/PrimeiroExemplo.java
+++ src/br/edu/estudos/PrimeiroExemplo.java
@@ -2,6 +2,6 @@ package br.edu.estudos;
 
 public class PrimeiroExemplo {
     public static void main(String[] args) {
-        System.out.println("Primeiro Exemplo!");
+        System.out.println(" -- - Primeiro Exemplo! -- -");
     }
 }
